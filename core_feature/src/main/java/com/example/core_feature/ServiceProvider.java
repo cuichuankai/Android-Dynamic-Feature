60751e11a98028937b868bc5721a5a5a15b0e06a
Farbod <farbod.ps@gmail.com>
2019-10-29 20:08:37 +0330

    add ServiceProvider

--- core_feature/src/main/java/com/example/core_feature/ServiceProvider.java
+++ core_feature/src/main/java/com/example/core_feature/ServiceProvider.java
@@ -1,4 +1,50 @@
 package com.example.core_feature;
 
-public class ServiceProvider {
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.core_feature.configuration.BaseConfiguration;
+
+public abstract class ServiceProvider extends ContentProvider {
+
+    @Override
+    public boolean onCreate() {
+        // cache context first, needed for initialization process
+        BaseConfiguration.init(getContext());
+
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
 }
